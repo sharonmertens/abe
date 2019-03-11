c331dcd33f8c75c6afb80a6100281464230de796
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-03-11 19:53:02 -0400

    added port

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const app = express();
 const db = mongoose.connection;
+
+// ------------------ //
+//        PORT        //
+// ------------------ //
+
+const PORT = process.env.PORT || 3000;
