7d7cf508a70c1fc9651bf24877862cbeca6fd5ca
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-03-15 21:07:35 -0400

    required and configured cors

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const app = express();
 const db = mongoose.connection;
+const cors = require('cors');
 
 // =======================================
 //             CONFIGURATION
@@ -32,6 +33,7 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cors());
 
 app.use(methodOverride('_method'));
 
